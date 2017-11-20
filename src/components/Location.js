fe2ab3ca1d4f383b5593c9e2be16bbc88df879e3
innyso <mini.inny@gmail.com>
2017-11-20 18:13:25 +1100

    add bluetooth status

--- src/components/Location.js
+++ src/components/Location.js
@@ -10,13 +10,39 @@ import {
   Text,
   View,
 } from 'react-native';
+import { BluetoothStatus } from 'react-native-bluetooth-status'
 
 export default class Location extends Component {
+
+  state = {
+    bluetoothState: ''
+  };
+
+  componentDidMount() {
+    this.updateBluetoothStatus();
+  }
+
+  async updateBluetoothStatus() {
+    try{
+      console.log("lollolololololololo");
+      console.log(BluetoothStatus);
+      const isEnabled = await BluetoothStatus.state();
+      console.log("isEnabled ", isEnabled);
+      this.setState({ bluetoothState: (isEnabled) ? 'On' : 'Off' });
+    } catch(err) {
+      console.error(error);
+    }
+
+  }
+
   render() {
     const { input } = this.props.navigation.state.params;
     return (
       <View style={styles.container}>
         <Text>{ input }</Text>
+        <Text style={styles.instructions}>
+          Bluetooth is: { this.state.bluetoothState }
+        </Text>
       </View>
     );
   }
@@ -27,5 +53,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#F5FCFF',
-  }
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
